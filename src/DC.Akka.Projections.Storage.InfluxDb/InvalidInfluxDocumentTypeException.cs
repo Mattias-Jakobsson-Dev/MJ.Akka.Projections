6678e33764380fcd8c73019503ac2905a8746060
Mattias Jakobsson <mattias@digitalcreation.se>
2024-04-08 16:10:25 +0200

    Removed unused code

--- src/DC.Akka.Projections.Storage.InfluxDb/InvalidInfluxDocumentTypeException.cs
+++
@@ -1,4 +0,0 @@
-namespace DC.Akka.Projections.Storage.InfluxDb;
-
-public class InvalidInfluxDocumentTypeException(Type requestedType) 
-    : Exception($"The requested type {requestedType} is not supported by the InfluxDb storage");
