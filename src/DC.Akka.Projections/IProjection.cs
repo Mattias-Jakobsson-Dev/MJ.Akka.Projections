6d5f336f12a65b4e89ded106cc61d4c0f84f0e2e
Mattias Jakobsson <mattias@digitalcreation.se>
2024-08-08 20:58:57 +0200

    Added default implimentation

--- src/DC.Akka.Projections/IProjection.cs
+++ src/DC.Akka.Projections/IProjection.cs
@@ -5,7 +5,7 @@ namespace DC.Akka.Projections;
 
 public interface IProjection<TId, TDocument> where TId : notnull where TDocument : notnull
 {
-    string Name { get; }
+    string Name => GetType().Name;
     ISetupProjection<TId, TDocument> Configure(ISetupProjection<TId, TDocument> config);
     Source<EventWithPosition, NotUsed> StartSource(long? fromPosition);
 }
