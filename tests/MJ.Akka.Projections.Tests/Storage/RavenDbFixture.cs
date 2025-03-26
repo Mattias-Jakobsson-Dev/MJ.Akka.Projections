0e835d167b49bba93e1abe9bbea05597258a9142
Mattias Jakobsson <mattias@digitalcreation.se>
2025-03-26 07:36:16 +0100

    Accept license

--- tests/MJ.Akka.Projections.Tests/Storage/RavenDbFixture.cs
+++ tests/MJ.Akka.Projections.Tests/Storage/RavenDbFixture.cs
@@ -1,5 +1,6 @@
 using JetBrains.Annotations;
 using Raven.Client.Documents;
+using Raven.Embedded;
 using Raven.TestDriver;
 
 namespace MJ.Akka.Projections.Tests.Storage;
@@ -7,6 +8,18 @@ namespace MJ.Akka.Projections.Tests.Storage;
 [PublicAPI]
 public class RavenDbFixture : RavenTestDriver
 {
+    static RavenDbFixture()
+    {
+        ConfigureServer(new TestServerOptions
+        {
+            Licensing = new ServerOptions.LicensingOptions
+            {
+                EulaAccepted = true,
+                ThrowOnInvalidOrMissingLicense = false
+            }
+        });
+    }
+    
     public IDocumentStore OpenDocumentStore()
     {
         return GetDocumentStore();
