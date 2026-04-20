3819f2c4f9f9bfd0561ca93f7fc66dd4f16e530e
Mattias Jakobsson <mattias@olka.se>
2026-04-20 16:07:54 +0200

    Added metadata

--- src/MJ.Akka.Projections/Documents/ModifyDocumentExtensions.cs
+++ src/MJ.Akka.Projections/Documents/ModifyDocumentExtensions.cs
@@ -42,4 +42,37 @@ public static class ModifyDocumentExtensions
         where TDocument : class
         where TContext : ContextWithDocument<TIdContext, TDocument>
         => setup.HandleWith((evnt, ctx, _, ct) => ctx.ModifyDocument(doc => modify(evnt, doc!, ct)));
+
+    public static ISetupEventHandlerForProjection<TIdContext, TContext, TEvent>
+        ModifyDocument<TIdContext, TDocument, TContext, TEvent>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent> setup,
+            Func<TEvent, TDocument, DocumentHandlingMetaData<TIdContext>, TDocument> modify)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.HandleWith((evnt, ctx, position, _) =>
+        {
+            ctx.ModifyDocument(doc => modify(evnt, doc!, new DocumentHandlingMetaData<TIdContext>(ctx.Id, position)));
+            return Task.CompletedTask;
+        });
+
+    public static ISetupEventHandlerForProjection<TIdContext, TContext, TEvent>
+        ModifyDocument<TIdContext, TDocument, TContext, TEvent>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent> setup,
+            Func<TEvent, TDocument, DocumentHandlingMetaData<TIdContext>, Task<TDocument>> modify)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.HandleWith((evnt, ctx, position, _) =>
+            ctx.ModifyDocument(doc => modify(evnt, doc!, new DocumentHandlingMetaData<TIdContext>(ctx.Id, position))));
+
+    public static ISetupEventHandlerForProjection<TIdContext, TContext, TEvent>
+        ModifyDocument<TIdContext, TDocument, TContext, TEvent>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent> setup,
+            Func<TEvent, TDocument, DocumentHandlingMetaData<TIdContext>, CancellationToken, Task<TDocument>> modify)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.HandleWith((evnt, ctx, position, ct) =>
+            ctx.ModifyDocument(doc => modify(evnt, doc!, new DocumentHandlingMetaData<TIdContext>(ctx.Id, position), ct)));
 }
