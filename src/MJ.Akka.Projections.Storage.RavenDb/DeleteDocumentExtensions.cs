ba9b73fecf7d118866e53bc0b7be4feb4673a505
Ludwig Weschke <ludwig@devease.se>
2026-04-22 12:55:02 +0200

    add ravendb delete document extensions

---
+++ src/MJ.Akka.Projections.Storage.RavenDb/DeleteDocumentExtensions.cs
@@ -0,0 +1,90 @@
+﻿using JetBrains.Annotations;
+using MJ.Akka.Projections.ProjectionIds;
+using MJ.Akka.Projections.Storage.RavenDb;
+
+// ReSharper disable once CheckNamespace
+namespace MJ.Akka.Projections.Documents;
+
+[PublicAPI]
+public static class RavenDbStorageDeleteDocumentExtensions
+{
+     public static ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData>
+        ConditionallyDeleteDocument<TDocument, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData> setup,
+            Func<TEvent, RavenDbProjectionContext<TDocument>, bool> condition)
+        where TDocument : class
+    {
+        setup.HandleWith((evnt, ctx, _, _, _) =>
+        {
+            if (condition(evnt, ctx)) ctx.DeleteDocument();
+            return Task.CompletedTask;
+        });
+        return setup;
+    }
+
+    public static ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData>
+        ConditionallyDeleteDocument<TDocument, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData> setup,
+            Func<TEvent, RavenDbProjectionContext<TDocument>, Task<bool>> condition)
+        where TDocument : class
+    {
+        setup.HandleWith(async (evnt, ctx, _, _, _) =>
+        {
+            if (await condition(evnt, ctx)) ctx.DeleteDocument();
+        });
+        return setup;
+    }
+
+    public static ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData>
+        ConditionallyDeleteDocument<TDocument, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData> setup,
+            Func<TEvent, RavenDbProjectionContext<TDocument>, CancellationToken, Task<bool>> condition)
+        where TDocument : class
+    {
+        setup.HandleWith(async (evnt, ctx, _, _, ct) =>
+        {
+            if (await condition(evnt, ctx, ct)) ctx.DeleteDocument();
+        });
+        return setup;
+    }
+
+    public static ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData>
+        ConditionallyDeleteDocument<TDocument, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData> setup,
+            Func<TEvent, TData, RavenDbProjectionContext<TDocument>, bool> condition)
+        where TDocument : class
+    {
+        setup.HandleWith((evnt, ctx, data, _, _) =>
+        {
+            if (condition(evnt, data, ctx)) ctx.DeleteDocument();
+            return Task.CompletedTask;
+        });
+        return setup;
+    }
+
+    public static ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData>
+        ConditionallyDeleteDocument<TDocument, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData> setup,
+            Func<TEvent, TData, RavenDbProjectionContext<TDocument>, Task<bool>> condition)
+        where TDocument : class
+    {
+        setup.HandleWith(async (evnt, ctx, data, _, _) =>
+        {
+            if (await condition(evnt, data, ctx)) ctx.DeleteDocument();
+        });
+        return setup;
+    }
+
+    public static ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData>
+        ConditionallyDeleteDocument<TDocument, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<SimpleIdContext<string>, TDocument, RavenDbProjectionContext<TDocument>, TEvent, TData> setup,
+            Func<TEvent, TData, RavenDbProjectionContext<TDocument>, CancellationToken, Task<bool>> condition)
+        where TDocument : class
+    {
+        setup.HandleWith(async (evnt, ctx, data, _, ct) =>
+        {
+            if (await condition(evnt, data, ctx, ct)) ctx.DeleteDocument();
+        });
+        return setup;
+    }
+}
