5e01ec08ed1943bc0d37b05b03c7f4ab7e805c69
Mattias Jakobsson <mattias@olka.se>
2026-04-22 13:30:35 +0200

    Moved delete document extensions

--- src/MJ.Akka.Projections/Documents/DeleteDocumentExtensions.cs
+++ src/MJ.Akka.Projections/Documents/DeleteDocumentExtensions.cs
@@ -80,4 +80,101 @@ public static class DeleteDocumentExtensions
         });
         return new SetupEventHandlerForContextWithDocument<TIdContext, TDocument, TContext, TEvent>(handler);
     }
+
+    public static ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData>
+        DeleteDocument<TIdContext, TDocument, TContext, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData> setup)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+    {
+        var handler = setup.HandleWith((_, context, _, _, _) => { context.DeleteDocument(); return Task.CompletedTask; });
+        return new SetupEventHandlerForContextWithDocument<TIdContext, TDocument, TContext, TEvent, TData>(handler);
+    }
+
+    public static ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData>
+        ConditionallyDeleteDocument<TIdContext, TDocument, TContext, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData> setup,
+            Func<TEvent, TContext, bool> condition)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.ConditionallyDeleteDocument((evnt, _, context, _, _) =>
+            Task.FromResult(condition(evnt, context)));
+
+    public static ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData>
+        ConditionallyDeleteDocument<TIdContext, TDocument, TContext, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData> setup,
+            Func<TEvent, TContext, Task<bool>> condition)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.ConditionallyDeleteDocument(async (evnt, _, context, _, _) =>
+            await condition(evnt, context));
+
+    public static ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData>
+        ConditionallyDeleteDocument<TIdContext, TDocument, TContext, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData> setup,
+            Func<TEvent, TData, TContext, bool> condition)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.ConditionallyDeleteDocument((evnt, data, context, _, _) =>
+            Task.FromResult(condition(evnt, data, context)));
+
+    public static ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData>
+        ConditionallyDeleteDocument<TIdContext, TDocument, TContext, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData> setup,
+            Func<TEvent, TData, TContext, long?, bool> condition)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.ConditionallyDeleteDocument((evnt, data, context, position, _) =>
+            Task.FromResult(condition(evnt, data, context, position)));
+
+    public static ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData>
+        ConditionallyDeleteDocument<TIdContext, TDocument, TContext, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData> setup,
+            Func<TEvent, TData, TContext, long?, Task<bool>> condition)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.ConditionallyDeleteDocument(async (evnt, data, context, position, _) =>
+            await condition(evnt, data, context, position));
+
+    public static ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData>
+        ConditionallyDeleteDocument<TIdContext, TDocument, TContext, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData> setup,
+            Func<TEvent, TData, TContext, Task<bool>> condition)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.ConditionallyDeleteDocument(async (evnt, data, context, _, _) =>
+            await condition(evnt, data, context));
+
+    public static ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData>
+        ConditionallyDeleteDocument<TIdContext, TDocument, TContext, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData> setup,
+            Func<TEvent, TData, TContext, CancellationToken, Task<bool>> condition)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+        => setup.ConditionallyDeleteDocument(async (evnt, data, context, _, ct) =>
+            await condition(evnt, data, context, ct));
+
+    public static ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData>
+        ConditionallyDeleteDocument<TIdContext, TDocument, TContext, TEvent, TData>(
+            this ISetupEventHandlerForContextWithExistingDocument<TIdContext, TDocument, TContext, TEvent, TData> setup,
+            Func<TEvent, TData, TContext, long?, CancellationToken, Task<bool>> condition)
+        where TIdContext : IProjectionIdContext
+        where TDocument : class
+        where TContext : ContextWithDocument<TIdContext, TDocument>
+    {
+        var handler = setup.HandleWith(async (evnt, context, data, position, cancellationToken) =>
+        {
+            if (await condition(evnt, data, context, position, cancellationToken))
+                context.DeleteDocument();
+        });
+        return new SetupEventHandlerForContextWithDocument<TIdContext, TDocument, TContext, TEvent, TData>(handler);
+    }
 }
